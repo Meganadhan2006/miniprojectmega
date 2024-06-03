73b4df3cf90b9e8e8e6caaa323a8f3818f8b8b2a
Meganadhan2006 <717823y125@kce.ac.in>
2024-06-03 16:34:40 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function Hello()
+{
+	var x={
+		name:"<h1><center><b>megha</b></center></h1><br>",
+		age:18,
+		clg:"<br>KCE"
+	};
+	document.write(x.name);
+	document.write(x.age);
+	document.write(x.clg);
+	
+}
